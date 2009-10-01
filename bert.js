d543cf0b6eecbcd6642526d7abb4f98387e84b45
Rusty Klophaus <rklophaus@gmail.com>
2009-10-01 08:02:51 -0400

    Merge bbrowning's changes. Add test cases for decoding floats and empty list.

--- bert.js
+++ bert.js
@@ -1,5 +1,6 @@
 // BERT-JS
 // Copyright (c) 2009 Rusty Klophaus (@rklophaus)
+// Contributions by Ben Browning (@bbrowning)
 // See MIT-LICENSE for licensing information.
 
 
@@ -431,6 +432,7 @@ BertClass.prototype.test_encode = function() {
 	alert(Bert.pp_bytes(Bert.encode([1, 2, 3])));
 	alert(Bert.pp_bytes(Bert.encode({a:1, b:2, c:3})));
 	alert(Bert.pp_bytes(Bert.encode(Bert.tuple("Hello", 1))));
+	alert(Bert.pp_bytes(Bert.encode([])));
 	alert(Bert.pp_bytes(Bert.encode({
 		a : Bert.tuple(1, 2, 3),
 		b : [4, 5, 6]
@@ -442,10 +444,18 @@ BertClass.prototype.test_decode = function() {
 	// Try decoding this: [{atom, myAtom},{binary, <<"My Binary">>},{bool, true}, {string, "Hello there"}],
 	TestTerm1 = Bert.bytes_to_string([131,108,0,0,0,4,104,2,100,0,4,97,116,111,109,100,0,6,109,121,65,116,111,109,104,2,100,0,6,98,105,110,97,114,121,109,0,0,0,9,77,121,32,66,105,110,97,114,121,104,2,100,0,4,98,111,111,108,100,0,4,116,114,117,101,104,2,100,0,6,115,116,114,105,110,103,107,0,11,72,101,108,108,111,32,116,104,101,114,101,106]);
 	alert(Bert.pp_term(Bert.decode(TestTerm1)));
-
+	
 	// Try decoding this: [{small_integer, 42},{integer1, 5000},{integer2, -5000},{big_int1, 987654321},{big_int2, -987654321}],
 	TestTerm2 = Bert.bytes_to_string([131,108,0,0,0,5,104,2,100,0,13,115,109,97,108,108,95,105,110,116,101,103,101,114,97,42,104,2,100,0,8,105,110,116,101,103,101,114,49,98,0,0,19,136,104,2,100,0,8,105,110,116,101,103,101,114,50,98,255,255,236,120,104,2,100,0,8,98,105,103,95,105,110,116,49,110,4,0,177,104,222,58,104,2,100,0,8,98,105,103,95,105,110,116,50,110,4,1,177,104,222,58,106]);
 	alert(Bert.pp_term(Bert.decode(TestTerm2)));
+	
+	// Try decoding this: -3.14159
+	TestTerm3 = Bert.bytes_to_string([131,99,45,51,46,49,52,49,53,56,57,57,57,57,57,57,57,57,57,57,56,56,50,54,50,101,43,48,48,0,0,0,0]);
+	alert(Bert.pp_term(Bert.decode(TestTerm3)));
+	
+	// Try decoding this: [] (empty list)
+	TestTerm4 = Bert.bytes_to_string([131,106]);
+	alert(Bert.pp_term(Bert.decode(TestTerm4)));
 }
 
 var Bert = new BertClass();
